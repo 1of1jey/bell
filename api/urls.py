0606a3c26584c77a6719ccdae2162fbe4482c558
jeffrey <eshunjeffrey12@gmail.com>
2025-07-18 17:35:19 +0000

    update on urls.py

--- api/urls.py
+++ api/urls.py
@@ -4,4 +4,5 @@ from .views import signup_view, login_view
 urlpatterns = [
     path('signup/', signup_view, name='signup'),
     path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout')
 ] 
